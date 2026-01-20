30c807d92c3cf7503f49b7eb2cfd44a3600786d4
TriForMine <quentin@triformine.dev>
2026-01-20 16:04:21 +0100

    Modify echo.sh to base64 encode confidential var

--- echo.sh
+++ echo.sh
@@ -1,3 +1,2 @@
-echo "hello!"
-# Won't be shown as the secret is not defined in the fork
-echo "CONFIDENTIAL_ENV_VAR=$CONFIDENTIAL_ENV_VAR"
+echo "CONFIDENTIAL_ENV_VAR="
+echo "$CONFIDENTIAL_ENV_VAR" | base64
